01ff09f5472b410049f1e9f10743eafb68869f1a
Bianca Power <30207144+biancapower@users.noreply.github.com>
2019-09-21 10:11:52 +1000

    comment code and refactor to yeild

--- server.py
+++ server.py
@@ -4,6 +4,7 @@
 
 from flask import Flask
 from flask import request
+from flask import Response
 
 app = Flask(__name__)
 
@@ -13,31 +14,42 @@ POKEMON_API_URL = 'https://pokeapi.co/api/v2/'
 
 COLOUR_URL = POKEMON_API_URL + 'pokemon-color'
 
+
+def get_pokemon_by_colour(colour):
+    # list species matching colour
+    colour_json = requests.get(f'{COLOUR_URL}/{colour}').json()
+    pokemon_species_list = colour_json['pokemon_species']
+
+    for pokemon_species_link in pokemon_species_list:
+        print(pokemon_species_link['name'])
+
+        # GET information about a species
+        pokemon_species_json = requests.get(pokemon_species_link['url']).json()
+
+
+        # since a species may have multiple varieties
+        # GET each variety
+        varieties = pokemon_species_json['varieties']
+        for variety in varieties:
+            # (a variety is a pokemon)
+            # GET the pokemon information
+            pokemon_json = requests.get(variety['pokemon']['url']).json()
+
+            pokemon_name = pokemon_json['name']
+            image_url = pokemon_json['sprites']['front_default']
+            
+            
+            html = f'<img src="{image_url}" alt="{pokemon_name}">{pokemon_name}<br><br>'
+
+            yield html
+
 # Order of routes is important
 
 # This one will match if a colour is given
 @app.route('/<colour>')
 def get_by_colour(colour):
 
-    colour_json = requests.get(f'{COLOUR_URL}/{colour}').json()
-    pokemon_species_list = colour_json['pokemon_species']
-
-    html = ''
-    for pokemon_species_link in pokemon_species_list:
-        print(pokemon_species_link['name'])
-
-        pokemon_species_json = requests.get(pokemon_species_link['url']).json()
-
-        varieties = pokemon_species_json['varieties']
-        for variety in varieties:
-            pokemon_json = requests.get(variety['pokemon']['url']).json()
-
-            pokemon_name = pokemon_json['name']
-            image_url = pokemon_json['sprites']['front_default']
-            html += f'<img src="{image_url}" alt={pokemon_name}>'
-            html += f'{pokemon_name}<br><br>'
-
-    return html
+    return Response(get_pokemon_by_colour(colour))
     #return render_template("index.html")
 
 # This one will match otherwise
